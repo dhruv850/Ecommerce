72af9f1b6894d3d2e6fb2969dd341e2b439860e1
Dhruv Patel <dhruv850.dp@gmail.com>
2020-06-02 22:38:17 -0400

    Commited First time

---
+++ routes/user.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    userById
+} =require('../controller/user');
+
+
+const {
+    requireSignin,isAuth, isAdmin
+} =require('../controller/auth');
+
+router.get('/secret/:userId',requireSignin,isAuth,isAdmin, (req,res) =>{
+res.json({
+    user: req.profile
+    });
+});
+router.param('userId',userById);
+
+
+module.exports= router;
