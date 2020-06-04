0176adbb3b500cdc77944013f7c0e16908721052
Dhruv Patel <dhruv850.dp@gmail.com>
2020-06-04 18:58:58 -0400

    Delete Product

--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controller/product");
+const { create, productById,read,remove } = require("../controller/product");
 const { requireSignin, isAuth, isAdmin } = require("../controller/auth");
 const { userById } = require("../controller/user");
 
+router.get('/product/:productId',read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
-
+router.delete('/product/:productId/:userId',requireSignin, isAuth, isAdmin,remove);
 router.param("userId", userById);
+router.param("productId", productById);
 
 module.exports = router;
